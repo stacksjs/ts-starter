6648ec81463a26cc55c658ef5890fce9ecbddc8d
Chris <chrisbreuer93@gmail.com>
2025-01-02 18:22:33 +0100

    chore: adjust eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,7 @@
+import type { ESLintConfig } from '@stacksjs/eslint-config'
 import stacks from '@stacksjs/eslint-config'
 
-export default stacks({
+const config: ESLintConfig = stacks({
   stylistic: {
     indent: 2,
     quotes: 'single',
@@ -13,3 +14,5 @@ export default stacks({
     'fixtures/**',
   ],
 })
+
+export default config
